07772f7c38652c01a3a8730acc1f1d73d8c75296
Junker <dk-junker@ya.ru>
2019-03-17 10:52:43 +0700

    version up

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "racket-route-match")
-(define version "0.0.5")
+(define version "0.0.6")
 (define deps '("base" "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/racket-route-match.scrbl" ())))
